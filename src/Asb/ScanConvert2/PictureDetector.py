ed65f5f05c332b51cc55bf6487c2ce3634a6b232
archivsozialebewegungen <admin@archivsozialebewegungen.de>
2023-01-06 12:18:43 +0100

    Added a picture detection service. Still needs testing with real material.

---
+++ src/Asb/ScanConvert2/PictureDetector.py
@@ -0,0 +1,67 @@
+'''
+This class tries to detect pictures in an image,
+because in binarization we want to treat pictures
+differently from text areas.
+
+The idea is: We use tesseracts segmentation algorithm
+to split the provided images into areas. And then
+we use the histogram of the different areas to determine,
+if this really is text or a picture in the text.
+
+Created on 06.01.2023
+
+@author: michael
+'''
+import pytesseract
+from PIL import Image
+from xml.dom.minidom import parseString
+import re
+import numpy as np
+from skimage.filters.thresholding import threshold_otsu
+
+class PictureDetector(object):
+    
+    def __init__(self):
+        
+        self.re_boundingbox = re.compile(r'bbox\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+).*')
+
+    def find_pictures(self, img: Image) -> Image:
+        
+        img = self._convert_to_bw(img)
+        pictures = []
+        for bbox in self._get_areas(img):
+            if self._is_image_area(bbox, img):
+                pictures.append(bbox)
+            
+        return pictures
+    
+    def _is_image_area(self, bbox, img: Image) -> bool:
+        
+        part = img.crop(bbox)
+        histogram = part.histogram()
+        ratio = 1.0 * histogram[0] / histogram[255]
+        return ratio > 0.2
+    
+    def _get_areas(self, img: Image) -> []:
+        
+        hocr = pytesseract.image_to_pdf_or_hocr(img, extension='hocr', lang="deu", config='--psm 1')
+        bboxes = []
+        dom = parseString(hocr)
+        with open("/tmp/hocr.xml", "w") as output:
+            output.write(dom.toprettyxml("  "))
+        for element in dom.getElementsByTagName("div"):
+            if element.getAttribute("class") == "ocr_carea":
+                matcher = re.match(self.re_boundingbox, element.getAttribute("title"))
+                if matcher:
+                    bboxes.append((int(matcher.group(1)),
+                                   int(matcher.group(2)),
+                                   int(matcher.group(3)),
+                                   int(matcher.group(4))))
+        return bboxes
+                                   
+    def _convert_to_bw(self, img: Image) -> Image:
+        
+        in_array = np.asarray(img.convert("L"))
+        mask = threshold_otsu(in_array)
+        out_array = in_array > mask
+        return Image.fromarray(out_array)
---
+++ tests/PictureDetectionTest.py
@@ -0,0 +1,20 @@
+'''
+Created on 06.01.2023
+
+@author: michael
+'''
+from Base import BaseTest
+from Asb.ScanConvert2.PictureDetector import PictureDetector
+from PIL import Image
+import os
+
+class PictureDetectionTest(BaseTest):
+
+    def testFile1(self):
+        
+        self.detector = PictureDetector()
+        img = Image.open(os.path.join(self.test_file_dir, "Singlefiles", "Seite8schraeg.jpg"))
+        for picture in self.detector.find_pictures(img):
+            blank = Image.new("1", (picture[2]-picture[0], picture[3]-picture[1]), color=0)
+            img.paste(blank, (picture[0], picture[1]))
+        img.save("/tmp/test.png")
